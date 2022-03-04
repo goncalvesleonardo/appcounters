82c8d94a4e68d389120b2bb55ea02874fec2d976
Leonardo Gonçalves <leonardo.goncalves@compasso.com.br>
2022-03-03 23:28:13 -0300

    added styled components

--- src/screens/counters/index.tsx
+++ src/screens/counters/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Text} from 'react-native';
 
+import {Container} from './styles';
+
 const Counters = () => {
   return (
-    <>
+    <Container>
       <Text>Counters</Text>
-    </>
+    </Container>
   );
 };
 
